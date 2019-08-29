f69e4e62885c0b36de2594e8973e346a1ce30978
nalbam <me@nalbam.com>
2019-08-29 13:43:53 +0900

    allin

--- modules/ecs/99-output.tf
+++ modules/ecs/99-output.tf
@@ -7,15 +7,3 @@ output "cluster_id" {
 output "cluster_name" {
   value = aws_ecs_cluster.cluster.name
 }
-
-output "vpc_id" {
-  value = data.aws_vpc.cluster.id
-}
-
-output "subnet_public_ids" {
-  value = aws_subnet.public.*.id
-}
-
-output "subnet_private_ids" {
-  value = aws_subnet.private.*.id
-}
