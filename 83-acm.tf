8133a1f746363ea182dd4512874c15d34f06191c
nalbam <me@nalbam.com>
2020-09-24 23:13:00 +0900

    acm

--- 83-acm.tf
+++ 83-acm.tf
@@ -10,24 +10,26 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "cert_validation" {
   count = var.base_domain != "" ? 1 : 0
 
-  zone_id = data.aws_route53_zone.app[0].id
-  name    = aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_type
-  ttl     = 60
+  zone_id = data.aws_route53_zone.app.0.id
+  name    = element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_name, [""]), 0)
+  type    = element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_type, [""]), 0)
+  ttl     = 300
 
   records = [
-    aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_value,
+    element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_value, [""]), 0),
   ]
 
-  depends_on = [aws_acm_certificate.cert]
+  depends_on = [
+    aws_acm_certificate.cert,
+  ]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   count = var.base_domain != "" ? 1 : 0
 
-  certificate_arn = aws_acm_certificate.cert[0].arn
+  certificate_arn = aws_acm_certificate.cert.0.arn
 
   validation_record_fqdns = [
-    aws_route53_record.cert_validation[0].fqdn,
+    aws_route53_record.cert_validation.0.fqdn,
   ]
 }
